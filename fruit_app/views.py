132c77b500eaea9bf09957caa73f40bf9252aeac
marioramirez90 <mario-ramirez@hotmail.de>
2026-07-16 14:39:52 +0200

    add send_fruits view with initial fruit data

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def send_fruits(request):
+   fruits =[
+      {
+        "name": "Apple",
+        "gewicht": 0.30,
+        "farbe": "rot",
+        },
+        {
+        "name": "Banana",
+        "gewicht": 0.20,
+        "farbe": "gelb",
+        },
+        {
+        "name": "Orange",
+        "gewicht": 0.25,
+        "farbe": "orange",
+        },
+        {
+        
+      }
+   ]
